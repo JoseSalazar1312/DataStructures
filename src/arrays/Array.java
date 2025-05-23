b4920900bb23953b2a564f8f128ae39a0988d06c
Jose_Salazar <js13122002@gmail.com>
2025-05-22 19:15:14 -0600

    Par o impar

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -1,6 +1,7 @@
 package arrays;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.SortedMap;
 
 public class Array {
     public static void main(String[] args) {
@@ -10,7 +11,8 @@ public class Array {
         do {
             System.out.println("\n=== MENÚ ===");
             System.out.println("1. Ingresar y mostrar números");
-            System.out.println("2. Salir");
+            System.out.println("2. Par o impar");
+            System.out.println("4. Salir");
             System.out.print("Elige una opción: ");
             opcion = entrada.nextInt();
 
@@ -19,12 +21,15 @@ public class Array {
                     ingresarYMostrarNumeros(entrada);
                     break;
                 case 2:
+                    parOImpar(entrada);
+                    break;
+                case 4:
                     System.out.println("Saliendo del programa...");
                     break;
                 default:
                     System.out.println("Opción no válida. Intenta de nuevo.");
             }
-        } while (opcion != 2);
+        } while (opcion != 4);
 
         entrada.close();
     }
@@ -43,6 +48,28 @@ public class Array {
         for (int num : array) {
             System.out.println(num);
         }
+    }
+
+    //Programa que pida 6 numeros enteros y posteriormente muestre los numeros con la palabra par o impar
+    public static void parOImpar(Scanner entrada){
+        int[] paroimpar = new int[6];
+
+        System.out.println("Ingresar 6 digitos: ");
+
+        for(int c = 0 ; c < 6; c++){
+            paroimpar[c] = entrada.nextInt();
+        }
+
+        for (int c = 0; c < 6; c++ ){
+            if (paroimpar[c] % 2 == 0) {
+                System.out.println(paroimpar[c] + " Par");
+            }else {
+                System.out.println(paroimpar[c] + " Impar");
+            }
+
+
+        }
+
     }
 
 }
