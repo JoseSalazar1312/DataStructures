c62a0fec348a574c9434c595ebf3f12441f564b1
Jose_Salazar <js13122002@gmail.com>
2025-05-22 18:30:17 -0600

    Opciones

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -1,24 +1,48 @@
 package arrays;
 import java.util.Scanner;
 import java.util.Arrays;
-// Introducir 5 números y mostrarlos en el mismo orden introduccido.
-
 
 public class Array {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int [] array = new int[5];
+        int opcion;
 
-        for(int c = 0; c < 5; c++){
-            System.out.println("Ingresar el digito " + (c + 1) + ": ");
+        do {
+            System.out.println("\n=== MENÚ ===");
+            System.out.println("1. Ingresar y mostrar números");
+            System.out.println("2. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ingresarYMostrarNumeros(entrada);
+                    break;
+                case 2:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+            }
+        } while (opcion != 2);
+
+        entrada.close();
+    }
+
+    public static void ingresarYMostrarNumeros(Scanner entrada) {
+        int[] array = new int[5];
+
+        for (int c = 0; c < 5; c++) {
+            System.out.print("Ingresar el número " + (c + 1) + ": ");
             array[c] = entrada.nextInt();
         }
-        System.out.println("Array es igual a: " + Arrays.toString(array));
 
-        System.out.println("Los numeros introducidos son: ");
-        for (int i = 0; i < array.length; i++){
-            System.out.println(array[i] + " ");
+        System.out.println("Array como texto: " + Arrays.toString(array));
+
+        System.out.println("Los números introducidos son:");
+        for (int num : array) {
+            System.out.println(num);
         }
-
     }
+
 }
